f81432161e2090dc0ffdbf5fd3372ed98f31c40b
mkarganov <maksim.karganov@gmail.com>
2014-04-21 23:14:27 +0300

    move template pattern to one file

--- 1_template/report.rb
+++ 1_template/report.rb
@@ -40,3 +40,55 @@ class Report
   end
 
 end
+
+class HtmlReport < Report
+  def output_start
+    puts('<html>')
+  end
+
+  def output_head
+    puts('  <head>')
+    puts("    <title>#{@title}</title>")
+    puts('  </head>')
+  end
+
+  def output_body_start
+    puts('<body>')
+  end
+
+  def output_line(line)
+    puts("  <p>#{line}</p>")
+  end
+
+  def output_body_end
+    puts('</body>')
+  end
+
+  def output_end
+    puts('</html>')
+  end
+end
+
+class PlainTextReport < Report
+  def output_start
+  end
+
+  def output_head
+    puts("**** #{@title} ****")
+    puts
+  end
+
+  def output_body_start
+  end
+
+  def output_line(line)
+    puts(line)
+  end
+
+  def output_body_end
+  end
+
+  def output_end
+  end
+
+end
