345195f71aa8ba7a5f70e8c6b97702b29b1cd792
mkarganov <maksim.karganov@gmail.com>
2014-04-25 15:52:46 +0300

    virtual proxy example

---
+++ 8_proxy/virtual_proxy.rb
@@ -0,0 +1,21 @@
+# proxy = VirtualProxy.new { BankAccount.new(50) }
+# proxy.deposit(25)
+# proxy.balance
+
+require './proxy_general.rb'
+
+class VirtualProxy
+  def initialize(&creation_block)
+    @creation_block = creation_block
+  end
+
+  def method_missing(name, *args)
+    s = subject
+    s.send(name, *args)
+  end
+
+  def subject
+    @subject = @creation_block.call unless @subject
+    @subject
+  end
+end
