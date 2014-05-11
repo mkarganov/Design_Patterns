b770828260ccb00d535d689e9eab2f23ccbc1d59
mkarganov <maksim.karganov@gmail.com>
2014-05-11 18:19:44 +0300

    builder pattern

---
+++ 12_builder/computer.rb
@@ -0,0 +1,45 @@
+class Computer
+  attr_accessor :display
+  attr_accessor :motherboard
+  attr_reader   :drives
+
+  def initialize(display=:crt, motherboard=Motherboard.new, drives = [])
+    @motherboard = motherboard
+    @drives = drives
+    @display = display
+  end
+end
+
+class CPU
+  # Common CPU stuff
+end
+
+class BasicCPU < CPU
+  # Lots of not very fast CPU-related stuff
+end
+
+class TurboCPU < CPU
+  # Lots of very fast CPU stuff
+end
+
+class Motherboard
+  attr_accessor :cpu
+  attr_accessor :memory_size
+
+  def initialize(cpu=BasicCPU.new, memory_size=1000)
+    @cpu = cpu
+    @memory_size = memory_size
+  end
+end
+
+class Drive
+  attr_reader :type # either :hard_disk, :cd, :dvd
+  attr_reader :size # in MB
+  attr_reader :writable # true, false
+
+  def initialize(type, size, writable)
+    @type = type
+    @size = size
+    @writable = writable
+  end
+end
